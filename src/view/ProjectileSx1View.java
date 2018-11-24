40465618cca7188bbce0cc154419131bd4c6e3f0
gabrielefantini <31799201+gabrielefantini@users.noreply.github.com>
2018-11-24 16:13:45 +0100

    commit

---
+++ src/view/ProjectileSx1View.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+/**
+ *
+ * @author gabri
+ */
+public class ProjectileSx1View extends ProjectileView{
+    public ProjectileSx1View(){
+        super();
+        imageNormal= new Image(ProjectileDxView.class.getResourceAsStream("/img/ProjectileSx1.png"));
+        this.imageDisplayed = new ImageView(imageNormal);
+        Space.getInstance().getChildren().add(imageDisplayed);
+    }
+    @Override
+    public void animationLoop(){
+        int c=2;
+        
+        int x = (int)imageDisplayed.getBoundsInParent().getMinX()-c;
+        int y =(int)imageDisplayed.getBoundsInParent().getMinY();
+        imageDisplayed.relocate(x,y);
+    }
+}
